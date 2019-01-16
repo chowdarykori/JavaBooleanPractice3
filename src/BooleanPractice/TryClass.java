0443b7c258eb0ba6d42995c05d0bf2bc38871989
unknown <skoripalli@gmail.com>
2019-01-16 00:33:43 +0000

    frist commit

---
+++ src/BooleanPractice/TryClass.java
@@ -0,0 +1,17 @@
+package BooleanPractice;
+
+public class TryClass {
+    public static void main(String[] args) {
+        boolean x=true; boolean y= false;
+        boolean a,b,c;
+        a=x&&y;
+        System.out.println(a);
+        b=x||y;
+        System.out.println(b);
+        c=!x;
+        System.out.println(c);
+
+
+
+    }
+}
